f983f4cef09d8e9e5d9ea3be4e5e4ef1185fa036
Pedro.Abadia <abadiapedro98@gmail.com>
2023-09-11 09:43:57 -0300

    Projeto curso Laravel:Criando uma aplicação MVC

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -27,7 +27,7 @@ class SeriesController extends Controller
         $serie = new Serie();
         $serie->nome = $nomeSerie;
         $serie ->save();
-        
+
         return redirect('/series');
     }
 }
